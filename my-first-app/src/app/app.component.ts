48ee6d0d5e642541011238c67ea7a1e374a800f5
rohitkolapkar <rohitkolapkar333@gmail.com>
2019-03-15 17:24:47 +0530

    section 2 : the basics

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -4,6 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  //style: [`
+    // h3{
+    //   color:SVGFEGaussianBlurElement;
+    // }
+  // `]
 })
 export class AppComponent {
   title = 'my-first-app';
---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer=false;
+  serverCreationStatus='No server was created';
+  serverName='Rk01';
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer=true;
+    },2000)
+   }
+
+  ngOnInit() {
+  }
+  onCreateServer(){
+    this.serverCreationStatus='server was created';
+  }
+
+  onUpdateServerName(event:Event){
+    this.serverName=(<HTMLInputElement>event.target).value;
+    console.log(event);
+  }
+
+}
